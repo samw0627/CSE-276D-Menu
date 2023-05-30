16de240b380d976e448a33c1ab6945fc61df9bd1
Sam Wong <97695769+samw0627@users.noreply.github.com>
2023-05-29 22:15:44 -0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,20 +4,28 @@ import pygame_menu
 pygame.init()
 surface = pygame.display.set_mode((600, 400))
 
-def set_difficulty(value, difficulty):
+
+def start_pain_scale():
     # Do the job here !
     pass
 
-def start_the_game():
+def start_bunny_game():
     # Do the job here !
     pass
 
-menu = pygame_menu.Menu('Welcome', 400, 300,
-                       theme=pygame_menu.themes.THEME_BLUE)
+def start_bunny_face():
+    # Do the job here !
+    pass
 
-menu.add.text_input('Name :', default='John Doe')
-menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
-menu.add.button('Pain Scale', start_the_game)
+menu = pygame_menu.Menu('BunnyBot', 600, 400,
+                       theme=pygame_menu.themes.THEME_SOLARIZED)
+HELP = 'Hello there! Select a mode to begin. ' \
+
+menu.add.label(HELP, max_char=-1, font_size=20)
+menu.add.button('Pain Scale', start_pain_scale)
+menu.add.button('Bunny Game', start_bunny_game)
+menu.add.button('Bunny Face', start_bunny_face)
+
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
 menu.mainloop(surface)
