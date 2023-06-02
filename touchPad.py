e040afdb61744910a5bc24c204939b42537d4b60
keli214 <77420536+keli214@users.noreply.github.com>
2023-06-02 15:52:45 -0700

    add inputs

--- touchPad.py
+++ touchPad.py
@@ -1,14 +1,17 @@
 import RPi.GPIO as GPIO  
 from time import sleep     # this lets us have a time delay (see line 12)  
 GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
-GPIO.setup(21, GPIO.IN)    # set GPIO 21 as input  
+GPIO.setup(21, GPIO.IN)    # set GPIO 21 as input 
+GPIO.setup(20, GPIO.IN)    # set GPIO 20 as input 
   
 try:  
     while True:            # this will carry on until you hit CTRL+C  
         if GPIO.input(21): # if port 21 == 1  
             print ("Port 21 is 1/GPIO.HIGH/True - button pressed")
+        elif GPIO.input(20): # if port 20 == 1  
+            print ("Port 20 is 1/GPIO.HIGH/True - button pressed")
         else:  
-            print ("Port 21 is 0/GPIO.LOW/False - button not pressed")  
+            print ("Noting is pressed")  
         sleep(0.1)         # wait 0.1 seconds  
   
 except KeyboardInterrupt:  
