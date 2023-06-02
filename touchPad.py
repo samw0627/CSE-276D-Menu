1026485495ee0e0e4e8c89ab5120e21f7a88835f
keli214 <77420536+keli214@users.noreply.github.com>
2023-06-02 13:46:58 -0700

    change print

--- touchPad.py
+++ touchPad.py
@@ -6,9 +6,9 @@ GPIO.setup(21, GPIO.IN)    # set GPIO 21 as input
 try:  
     while True:            # this will carry on until you hit CTRL+C  
         if GPIO.input(21): # if port 21 == 1  
-            print ("Port 25 is 1/GPIO.HIGH/True - button pressed")
+            print ("Port 21 is 1/GPIO.HIGH/True - button pressed")
         else:  
-            print ("Port 25 is 0/GPIO.LOW/False - button not pressed")  
+            print ("Port 21 is 0/GPIO.LOW/False - button not pressed")  
         sleep(0.1)         # wait 0.1 seconds  
   
 except KeyboardInterrupt:  
