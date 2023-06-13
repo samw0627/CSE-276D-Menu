923ebc2cb150d212aea8dff8ec778c2a5a4c9105
Justin Nguyen <justnguyen1@gmail.com>
2023-06-13 16:04:26 -0700

    finished commenting bunny jumping game

--- bunnyjump.py
+++ bunnyjump.py
@@ -106,34 +106,40 @@ class Bunny:
         if self.bunny_jump:
             self.jump()
         
-        if self.step_index >= 15:
+        # check for cycling through the bunny running animations
+        if self.step_index >= 15: 
             self.step_index = 0
         
+        # if the screen is touched or fabric is touched and the bunny is not already jumping
         if (fabric or userInput[0]) and not self.bunny_jump:
-            pygame.mixer.Sound.play(JUMP_SOUND)
+            pygame.mixer.Sound.play(JUMP_SOUND) # play the jumping sound
             pygame.mixer.music.stop()
             self.bunny_run = False
             self.bunny_jump = True
+        # if the bunny is not jumping or the screen is not touched or the fabric is not touched, keep running
         elif not (self.bunny_jump or userInput[0] or fabric):
             self.bunny_run = True
             self.bunny_jump = False
     
+    # displaying the bunny while it is running
     def run(self):
-      self.image = self.run_img[self.step_index // 5]
+      self.image = self.run_img[self.step_index // 5] # changing the image for the running animation
       self.bunny_rect = self.image.get_rect()
       self.bunny_rect.x = self.X_POS
       self.bunny_rect.y = self.Y_POS
       self.step_index += 1
     
+    # defining the jumping action of the bunny
     def jump(self):
         self.image = self.jump_img
         if self.bunny_jump:
-            self.bunny_rect.y -= self.jump_vel * 3
-            self.jump_vel -= 0.849
+            self.bunny_rect.y -= self.jump_vel * 3 # move the bunny into the air
+            self.jump_vel -= 0.849 # decrease the rate at which the bunny is moving into the air
         if self.jump_vel < -self.JUMP_VEL:
-            self.bunny_jump = False
-            self.jump_vel = self.JUMP_VEL
+            self.bunny_jump = False # making the bunny fall back down from jumping
+            self.jump_vel = self.JUMP_VEL # resetting the jump velocity
 
+    # drawing the bunny on the game screen
     def draw(self, screen):
       screen.blit(self.image, (self.bunny_rect.x, self.bunny_rect.y))
 
@@ -141,8 +147,8 @@ def main():
     global game_speed, x_pos_bg, y_pos_bg, points, snakes
     run = True
     clock = pygame.time.Clock()
-    player = Bunny()
-    cloud = Cloud()
+    player = Bunny() # instance of the bunny the player will play as
+    cloud = Cloud() # instance of the cloud moving across the background
     game_speed = 10
     x_pos_bg = 0
     y_pos_bg = 348
@@ -157,15 +163,18 @@ def main():
     def score():
         global points, game_speed
         points += 1
+        # increase the speed of the game every 500 points
         if points % 500 == 0:
             game_speed += 1
 
+        # displaying the points scored so far in the top right of the game screen
         text = font.render("Points: " + str(points), True, (0, 0, 0))
         textRect = text.get_rect()
         textRect.center = (900, 40)
         SCREEN.blit(text, textRect)
 
     def background():
+        # creating the ground that the bunny will be running on
         global x_pos_bg, y_pos_bg
         image_width = BG[0].get_width()
         for i in range(0, 60):
@@ -174,13 +183,15 @@ def main():
                 SCREEN.blit(BG[1], (image_width*i + x_pos_bg, image_width*j + y_pos_bg))
         if x_pos_bg <= -image_width:
             x_pos_bg = 0
-        x_pos_bg -= game_speed
+        x_pos_bg -= game_speed # moving the ground across the screen
 
     while run:
         for event in pygame.event.get():
+            # exiting the game
             if event.type == pygame.QUIT:
                 run = False
                 sys.exit()
+            # if the menu button is pressed, go to the main menu
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = event.pos  # gets mouse position
                 if menuButtonObj.button.collidepoint(mouse_pos):
@@ -188,25 +199,27 @@ def main():
                     mainMenu.main()
         
         SCREEN.fill((135, 206, 235))
-        userInput = pygame.mouse.get_pressed()
+        userInput = pygame.mouse.get_pressed() # get whether or not the screen was touched
         fabric = False
-        if GPIO.input(20):
+        if GPIO.input(20): # check whether the conductive fabric was touched
             fabric = True
         player.draw(SCREEN)
         player.update(userInput, fabric)
 
+        # if there are no more snakes, create a new one and add it to the list of snakes
         if len(snakes) == 0:
             snakes.append(Snake())
         
         for obstacle in snakes:
             obstacle.draw(SCREEN)
             obstacle.update()
+            # if the bunny collides with the snake, the game ends
             if player.bunny_rect.colliderect(obstacle.rect):
                 pygame.time.delay(1000)
                 death_count += 1
                 menu(death_count)
 
-        background()
+        background() 
         cloud.draw(SCREEN)
         cloud.update()
         score()
@@ -226,13 +239,19 @@ def menu(death_count):
         font = pygame.font.Font('freesansbold.ttf', 30)
 
         if death_count == 0:
+            # rendering the UI for the very beginning 
             text = font.render("Press screen to Start", True, (0, 0, 0))
         else:
+            # rendering the UI and displaying the score for when the player collides with a snake
             text = font.render("Press screen to Restart", True, (0, 0, 0))
             score = font.render("Your Score: " + str(points), True, (0, 0, 0))
+
+            # positioning the score in the corner
             scoreRect = score.get_rect()
             scoreRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 100)
             SCREEN.blit(score, scoreRect)
+        
+        # displaying the instructions on the screen
         instruction = font.render("Touch the screen to jump over the snake", True, (0, 0, 0))
         instructionRect = instruction.get_rect()
         instructionRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
@@ -248,8 +267,6 @@ def menu(death_count):
             if event.type == pygame.QUIT:
                 run = False
                 sys.exit()
-            # if event.type == pygame.KEYDOWN:
-            #     main()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = event.pos 
                 if menuButtonObj.button.collidepoint(mouse_pos):
